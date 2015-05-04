69fd454ce1b3cf5949ab6a7b83fa197fd2e16bd0
nmormando <student@nmormando.local>
2015-05-04 12:23:16 -0500

    wardrobe 2

--- mywardrobe2/ViewController.swift
+++ mywardrobe2/ViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var wardrobeButton: UILabel!
+    @IBOutlet weak var outfitsButton: UILabel!
+    @IBOutlet weak var starredButton: UILabel!
+    @IBOutlet weak var usedButton: UILabel!
+   
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
